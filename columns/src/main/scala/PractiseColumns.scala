940b6c5cfa9298e47ab750d1dca6d1186f027af3
Arnaud L <3510366-Nonontb@users.noreply.gitlab.com>
2021-12-12 18:57:53 +0100

    Implemented solution

--- columns/src/main/scala/PractiseColumns.scala
+++ columns/src/main/scala/PractiseColumns.scala
@@ -6,28 +6,28 @@ import javax.xml.crypto.Data
 
 object PractiseColumns {
   // Create a col from a literal
-  def createConst(df:DataFrame,colName:String,value:Any):DataFrame = ???
+def createConst(df:DataFrame,colName:String,value:Any):DataFrame = df.withColumn(colName,lit(value))
 
-  //replace df value col per a padded one : use lpad
-  def replaceWithPaddedCol(df:DataFrame,colName:String, len:Int,paddedChar:String):DataFrame = ???
+//replace df value col per a padded one : use lpad
+def replaceWithPaddedCol(df:DataFrame,colName:String, len:Int,paddedChar:String):DataFrame = df.withColumn(colName,lpad(col(colName),len,"0"))
 
-  // create a col fullName from firstName and lastName: use concat
-  def concatColumns(df:DataFrame, newColName:String, srcCols:Seq[Column]):DataFrame = ???
+// create a col fullName from firstName and lastName: use concat
+def concatColumns(df:DataFrame, newColName:String, srcCols:Seq[Column]):DataFrame = df.withColumn(newColName,concat(srcCols:_*))
 
-  // convert value (date as str) to date type : use to_date
-  def dateStrAsDate(df:DataFrame,dateAsStrCol:String):DataFrame = ???
+// convert value (date as str) to date type : use to_date
+def dateStrAsDate(df:DataFrame,dateAsStrCol:String):DataFrame = df.withColumn(dateAsStrCol,to_date(col(dateAsStrCol),"dd-MM-yyyy"))
 
-  //convert tab value field into mulitple lines
-  def explodeFieldIntoLine(df:DataFrame,tabColName:String,explodedColName:String):DataFrame = ???
+//convert tab value field into mulitple lines
+def explodeFieldIntoLine(df:DataFrame,tabColName:String,explodedColName:String):DataFrame = df.withColumn(explodedColName,explode(col(tabColName)))
 
-  //convert  value field into list of all fields value: use collect_list 
-  def collectField(df:DataFrame,colName:String,collectedColName:String):DataFrame = ???
+//convert  value field into list of all fields value: use collect_list 
+def collectField(df:DataFrame,colName:String,collectedColName:String):DataFrame = df.select(collect_list(colName).as(collectedColName))
 
-  //copy id from root into a existing nested struct nested.id: use withField
-  def addNestedField(df:DataFrame,colName:String):DataFrame = ???
+//copy id from root into a existing nested struct nested.id
+def addNestedField(df:DataFrame,colName:String):DataFrame = df.withColumn("nested",col("nested").withField(colName,col(colName)))
 
-  //delete existing nested.toReplaceColName by id from root: use dropField
-  def deleteNestedField(df:DataFrame,toDeleteColName:String):DataFrame = ???
+//delete existing nested.toReplaceColName by id from root
+def deleteNestedField(df:DataFrame,toDeleteColName:String):DataFrame = df.withColumn("nested",col("nested").dropFields(toDeleteColName))
 
 
 }
